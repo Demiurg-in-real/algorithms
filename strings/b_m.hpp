0539a7b4cc966c43b0094f4fe9af71e3c4c2847b
Demiurg-in-real <bezginandrew@gmail.com>
2020-06-16 16:53:49 +0300

    Added to named space of string algorithms.

--- strings/b_m.hpp
+++ strings/b_m.hpp
@@ -1,15 +1,17 @@
 #pragma once
 #include<vector>
 #include<string>
-class bm{
-	private:
-		size_t pos;
-		std::vector<std::pair<char,int>> stop;
-		std::vector<std::pair<std::string, unsigned int>> suf;
-		void make_suf_stop(const std::string& ustr);
-		void clear();
-	public:
-		bm();
-		void search(const std::string& main, const std::string& submain, std::vector<int>& positions);
-};
-#include "b_m.tpp"
+namespace ALGS{
+	class bm{
+		private:
+			size_t pos;
+			std::vector<std::pair<char,int>> stop;
+			std::vector<std::pair<std::string, unsigned int>> suf;
+			void make_suf_stop(const std::string& ustr);
+			void clear();
+		public:
+			bm();
+			void search(const std::string& main, const std::string& submain, std::vector<int>& positions);
+	};
+	#include "b_m.tpp"
+}
