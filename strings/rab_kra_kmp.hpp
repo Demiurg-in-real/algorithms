b29b87338e964bcfed30e3d0c8a15bae8677ab82
Demiurg-in-real <bezginandrew@gmail.com>
2020-06-16 16:53:49 +0300

    Additionally added to its named space of string algorithms.

--- strings/rab_kra_kmp.hpp
+++ strings/rab_kra_kmp.hpp
@@ -3,6 +3,8 @@
 #include<string>
 #include<cmath>
 #include "prefix.hpp"
-void rabin_kramp(const std::string& str, const std::string& substr, std::vector<int>& pos);
-void KMP(const std::string& str, const std::string& substr, std::vector<int>& pos);
-#include "rab_kra_kmp.tpp"
+namespace ALGS{
+	void rabin_kramp(const std::string& str, const std::string& substr, std::vector<int>& pos);
+	void KMP(const std::string& str, const std::string& substr, std::vector<int>& pos);
+	#include "rab_kra_kmp.tpp"
+}
